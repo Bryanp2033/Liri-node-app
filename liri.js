8f73ee2e60045cb011b32c073783dbf813474478
Bryan <bryanporras2033@gmail.com>
2017-09-26 00:19:52 -0400

    made changes to package.json & got movie-this command working

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
-var key = require("./keys.js")
+var request = require('request');
+var key = require("./keys.js");
 
 var consumer_key = key.twitterKeys.consumer_key;
 var consumer_secret = key.twitterKeys.consumer_secret;
@@ -9,10 +10,50 @@ var client_id = key.spotifyKeys.client_id;
 var client_secret = key.spotifyKeys.client_secret;
 
 
-// function Display(request, input){
+var keyword = process.argv[2];
+var input = process.argv[3];;
+
+    switch(keyword){
+        case 'movie-this':
+        var movie_title = input
+
+        var movie_url = "http://www.omdbapi.com/?t=" + movie_title +"&plot=short&apikey=40e9cece"
+        console.log(movie_url)
+            request(movie_url, function(error, response, body){
+
+                if(error){
+                    return error;
+                }
+          
+                  if(!error && response.statusCode == "200"){
+          
+                    var movie = JSON.parse(body)
+                    
+                    console.log(" ")
+                    console.log("Title: " + movie.Title)
+                    console.log("____________________________________")
+                    console.log(" ")
+                    console.log("Year: " + movie.Year)
+                    console.log("____________________________________")
+                    console.log(" ")
+                    console.log("Imdb Rating:" + movie.imdbRating)
+                    console.log("____________________________________")
+                    console.log(" ")
+                    console.log("Rotten Tomatoes Rating: " + movie.Ratings[1].Value)
+                    console.log("____________________________________")
+                    console.log(" ")
+                    console.log("Language: " + movie.Language)
+                    console.log("____________________________________")
+                    console.log(" ")
+                    console.log("Movie Plot: " + movie.Plot)
+                    console.log("____________________________________")
+                    console.log(" ")
+                    console.log("Actors: " + movie.Actors)
+                    console.log("____________________________________")
+                  }
+                })
+        break;
+    }
 
 
-//     switch(request){
-//         case
-//     }
-// }
+
