88b78c8581541f3b550eb3ed2161f260b324fd76
Bryan <bryanporras2033@gmail.com>
2017-09-25 22:12:27 -0400

    downloaded packages and test if keys worked in liri.js

---
+++ liri.js
@@ -0,0 +1,18 @@
+var key = require("./keys.js")
+
+var consumer_key = key.twitterKeys.consumer_key;
+var consumer_secret = key.twitterKeys.consumer_secret;
+var access_token_key = key.twitterKeys.access_token_key;
+var access_token_secret =  key.twitterKeys.access_token_secret;
+
+var client_id = key.spotifyKeys.client_id;
+var client_secret = key.spotifyKeys.client_secret;
+
+
+// function Display(request, input){
+
+
+//     switch(request){
+//         case
+//     }
+// }
